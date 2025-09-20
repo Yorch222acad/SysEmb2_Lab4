8e20661726239373cda1fbf8ac7f8cd904fb1b75
Jorge Salgueiro <yorch.academico@gmail.com>
2025-09-19 22:52:20 -0400

    Ejr 1,2,3 Tiva

---
+++ L4ejr1T/gcc/L4ejr1T.d
@@ -0,0 +1,7 @@
+gcc/L4ejr1T.o: L4ejr1T.c \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ ../../../../inc/hw_ints.h ../../../../inc/hw_memmap.h \
+ ../../../../inc/hw_types.h ../../../../driverlib/gpio.h \
+ ../../../../driverlib/interrupt.h ../../../../driverlib/sysctl.h \
+ ../../../../driverlib/timer.h
---
+++ L4ejr2T/gcc/L4ejr2T.d
@@ -0,0 +1,7 @@
+gcc/L4ejr2T.o: L4ejr2T.c \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ ../../../../inc/hw_ints.h ../../../../inc/hw_memmap.h \
+ ../../../../inc/hw_types.h ../../../../driverlib/gpio.h \
+ ../../../../driverlib/interrupt.h ../../../../driverlib/sysctl.h \
+ ../../../../driverlib/timer.h
