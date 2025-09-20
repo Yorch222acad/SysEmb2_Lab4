4ec0534bf4ebe404955fc805b05c4f2003c6aaa6
Jorge Salgueiro <yorch.academico@gmail.com>
2025-09-20 11:58:52 -0400

    Quitar archivos sobrantes

--- L4ejr3T/gcc/L4ejr3T.d
+++
@@ -1,7 +0,0 @@
-gcc/L4ejr3T.o: L4ejr3T.c \
- /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
- /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
- ../../../../inc/hw_ints.h ../../../../inc/hw_memmap.h \
- ../../../../inc/hw_types.h ../../../../driverlib/gpio.h \
- ../../../../driverlib/interrupt.h ../../../../driverlib/sysctl.h \
- ../../../../driverlib/timer.h
